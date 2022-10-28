d0902a71b8efd530e132d206802807399ac21de6
rhkzl6 <rhkzl6@gmail.com>
2022-10-28 15:35:53 +0900

    backend test

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ cors = CORS(app, resources={"r/*": {"origins": "*"}})
 
 @app.route("/hello")
 def hello():
-    result = {"code" : 200, "message" : "hello flask"}
-    return result
+    code = 200
+    message = "hello flask"
+    # result = {"code" : 200, "message" : "hello flask"}
+    return code, message
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
