7fc1610d1dd4fd67dc83d9f0aea192c3f75e8f9f
rhkzl6 <rhkzl6@gmail.com>
2022-11-01 10:16:09 +0900

    backend test

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from flask import Flask
 from flask_cors import CORS
+import pymysql
+import os
+
 
 app = Flask(__name__)
-cors = CORS(app, resources={"r/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/hello")
 def hello():
-    result = {"code" : 200, "message" : "hello flask"}
+    host = os.environ['DB_HOST']
+    user = os.environ['DB_USER']
+    password = os.environ['MYSQL_ROOT_PASSWORD']
+    db = os.environ['DB_NAME']
+    conn = pymysql.connect(host=host, user=user, db=db,
+                           password=password, charset='utf8')
+    curs = conn.cursor()
+    sql = "select * from student";
+    curs.execute(sql)
+    rows = curs.fetchall()
+    print(rows)
+    conn.commit()
+    conn.close()
+
+    result = {"code": 200, "message": rows}
     return result
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
 
-    #dafafsafaaaaddsas
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
